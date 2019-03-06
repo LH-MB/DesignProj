7b9b08ef01ea05fd97cb5759682c92783cce1545
LH-MB <48165566+LH-MB@users.noreply.github.com>
2019-03-06 17:01:46 -0500

    Checkpoint and RaceCar objects

---
+++ Checkpoint.java
@@ -0,0 +1,67 @@
+
+//Louis Hwang
+
+//Design Project
+
+//Checkpoint 
+public class Checkpoint{
+  
+  private double xPos;
+  private double yPos;
+
+  
+  //Default constructor
+  public Checkpoint(){
+    xPos = 0.0;
+    yPos = 0.0;
+  }
+  
+  public Checkpoint(double xPos, double yPos){
+    this.xPos = xPos;
+    this.yPos = yPos;
+  }
+  
+  //Getters
+  public double getXPos(){
+    return xPos;
+  }
+  
+  public double getYPos(){
+    return yPos;
+  }
+
+  //Setters
+  public void setXPos(double a){
+    xPos = a;
+  }
+  
+  public void setYPos(double a){
+    yPos = a;
+  }
+  
+  public String toString(){
+    return "X Position: " + xPos + " Y Position: " + yPos;
+    
+  }
+  
+  public boolean equals(Object obj){
+    if (obj == this) return true;
+    
+    if (obj == null) return false;
+    
+    if (this.getClass() == obj.getClass()){
+      Checkpoint a = (Checkpoint) obj;
+      
+      return this.xPos == a.xPos
+      && this.yPos == a.yPos;
+    }
+    else
+      return false;
+  }
+
+ 
+  
+  
+  
+  
+}
---
+++ RaceCar.java
@@ -0,0 +1,115 @@
+
+//Louis Hwang
+
+//Design Project
+
+//RaceCar
+public class RaceCar{
+  
+  private int engine;
+  private int tires;
+  private boolean nitrous;
+  private String color;
+  private char currentPosition;
+  private char endPosition;
+  
+  //Default constructor
+  public RaceCar(){
+    engine = 0;
+    tires = 0;
+    nitrous = false;
+    color = null;
+    currentPosition = '\0';
+    endPosition = '\0';
+  }
+  
+  public RaceCar(int engine, int tires, boolean nitrous, String color, char currentPosition, char endPosition){
+    this.engine = engine;
+    this.tires = tires;
+    this.nitrous = nitrous;
+    this.color = color;
+    this.currentPosition = currentPosition;
+    this.endPosition = endPosition;
+  }
+  
+  //Getters
+  public int getEngine(){
+    return engine;
+  }
+  
+  public int getTires(){
+    return tires;
+  }
+  
+  public boolean getNitrous(){
+    return nitrous;
+  }
+  
+  public String getColor(){
+    return color;
+  }
+  
+  public char getCurrentPosition(){
+    return currentPosition;
+  }
+  
+  public char getEndPosition(){
+    return endPosition;
+  }
+  
+  //Setters
+  public void setEngine(int a){
+    engine = a;
+  }
+  
+  public void setTires(int a){
+    tires = a;
+  }
+  
+  public void setNitrous(boolean a){
+    nitrous = a;
+  }
+  
+  public void setCurrentPosition(char a){
+    currentPosition = a;
+  }
+  
+  public void setEndPosition(char a){
+    endPosition = a;
+  }
+  
+  public void randomizeValues(){
+    
+  }
+  
+  public String toString(){
+    return "Engine: " + engine + " Tires: " + tires + " Nitrous: " + nitrous + " Color: " + color + " Current Position: " + currentPosition
+    + "End Position " + endPosition;
+    
+  }
+  
+  public boolean equals(Object obj){
+    if (obj == this) return true;
+    
+    if (obj == null) return false;
+    
+    if (this.getClass() == obj.getClass()){
+      RaceCar a = (RaceCar) obj;
+      
+      return this.engine == a.engine 
+      && this.tires == a.tires 
+      && this.nitrous == a.nitrous 
+      && this.color.equals(a.color) 
+      && this.currentPosition == a.currentPosition 
+      && this.endPosition == a.endPosition;
+    }
+    else
+      return false;
+  }
+
+ 
+  
+  
+  
+  
+}
