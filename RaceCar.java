8841da291cf27ce650974136c36f5cf9bf807f46
LH-MB <48165566+LH-MB@users.noreply.github.com>
2019-03-13 12:44:52 -0400

    Update RaceCar.java

--- RaceCar.java
+++ RaceCar.java
@@ -8,11 +8,11 @@ import javafx.scene.image.ImageView;
  * CS 225 - Project 3
  * Louis created engine, tires, nitrous, startPosition, endPosition, toString, and equals attributes/methods
  * Nathan changed data types of some attributes. Created colorCar, calculatedSpeed, ImageView attributes/methods
+ * //RaceCar class represents a RaceCar. Attributes are engine, tires, nitrous, calculated speed, start position, end position, and image;
  */
 
 @SuppressWarnings("Duplicates")
 
-//RaceCar class represents a RaceCar on a track. Attributes are engine, tires, nitrous, calculated speed, start position, end position, and image;
 public class RaceCar{
     
     private String colorCar;
